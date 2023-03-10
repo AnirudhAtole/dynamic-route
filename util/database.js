2260cbf128f61de23f7be09e9827acd211ffc3f6
AnirudhAtole <anirudhatole@gmail.com>
2023-03-10 18:30:07 +0530

    made cart and controller

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
 const Sequalize = require('sequelize');
 
-const sequelize = new Sequalize('node-complete','root','Asparagus',{dialect : 'mysql' , host:'localhost'});
+const sequelize = new Sequalize('shop-project','root','Asparagus',{dialect : 'mysql' , host:'localhost'});
 
 module.exports = sequelize;
